55c99ab04fbe07a0fb0cb998e61cd152bea0081b
khoahd7621 <hoangdangkhoa7621@gmail.com>
2023-08-17 21:18:57 +0700

    Update cd pipeline

--- cd/vars/releaseBackEnd.groovy
+++ cd/vars/releaseBackEnd.groovy
@@ -6,6 +6,7 @@ void call() {
     String awsRegion = "us-east-1"
     String eksName = "practical-devops-sd5368-eks"
     String ecrRegistryUrl = "https://218220206490.dkr.ecr.us-east-1.amazonaws.com"
+    String awsCredential = 'aws-credentials'
 
 //========================================================================
 //========================================================================
@@ -19,7 +20,7 @@ void call() {
     }
 
     stage ("Deploy BackEnd To K8S") {
-        script {
+        withAWS(credentials: "${awsCredential}", region: "${awsRegion}") {
             sh "aws ecr get-login-password --region ${awsRegion} | docker login --username AWS --password-stdin ${demoRegistry}"
             sh "export registry=${demoRegistry}; export appname=${name}; export tag=latest; \
             envsubst < .ci/service/deployment.yml > deployment.yml"
